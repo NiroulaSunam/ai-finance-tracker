0170de060d602cef5faa7a449a42b71d49e38336
Sunam Niroula <69410705+NiroulaSunam@users.noreply.github.com>
2026-03-11 16:53:16 +0545

    feat: zod schema handling

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
-export default function Signup() {
-  return (
-    <main>
-      <h1> This is signup </h1>
-    </main>
-  );
-}
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useRouter } from 'next/navigation';
+import { createClient } from '@/lib/supabase/client';
+import { signupSchema } from '@/lib/validations/auth';
+
+type SignupFormData = z.infer<typeof signupSchema>;
+
+export default function SignupPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignupFormData>({
+    resolver: zodResolver(signupSchema),
+  }
+
---
+++ src/lib/validations/auth.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+    firstname: z.string().min(1, "First name is required"),
+    lastname: z.string().min(1, "Last name is required"),
+    email: z.email("Invalid email address"),
+    password: z.string()
+    .min(8, 'Min 8 characters')
+    .regex(/[A-Z]/, 'Must contain uppercase')
+    .regex(/[0-9]/, 'Must contain a number')
+    .regex(/[^a-zA-Z0-9]/, 'Must contain a special character'),
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+});
